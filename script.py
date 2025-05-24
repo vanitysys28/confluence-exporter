018c5a7d16c6f273e022f614aeb3bff9d673ab22
Guillaume Aubert <gaubert@centreon.com>
2025-05-24 19:36:50 +0200

    import data from Confluence API

--- script.py
+++ script.py
@@ -13,23 +13,23 @@ pages = []
 path = []
 
 def getConfluenceData():
-    json = []
-
     url = os.getenv('URL')
-    endpoint = '/wiki/api/v2/spaces/2564620362/pages?body-format=storage&limit=250&cursor=eyJpZCI6IjI2NzQwMzI2OTgiLCJjb250ZW50T3JkZXIiOiJpZCIsImNvbnRlbnRPcmRlclZhbHVlIjoyNjc0MDMyNjk4fQ=='
+    endpoint = '/wiki/api/v2/spaces/2564620362/pages?body-format=storage&limit=250'
     auth = HTTPBasicAuth(os.getenv('EMAIL'), os.getenv('TOKEN'))
     
     response = requests.get(url + endpoint,auth=auth)
     data = response.json()
-
-    json.append(data['results'])
-
+    
     if(data['_links']['next']):
-        response = requests.get(url + data['_links']['next'])
-        data = response.json()
-        json.append(data['results'])
+        response = requests.get(url + data['_links']['next'], auth=auth)
+        data['results'].extend(response.json()['results'])
 
-    return json
+    return data
+
+def filterParentPages(data):
+    for i in data['results']:
+        if i['status']!='archived' and i['parentId'] not in parent_ids: 
+            parent_ids.append(i['parentId'])
 
 def filterBadCharacters(string):
     characters = [':','/','<','>','*']
@@ -38,33 +38,30 @@ def filterBadCharacters(string):
             string.replace(character,'-')
     return string
 
-def getParentTitle(parent_id):
+def getParentTitle(data, parent_id):
     for i in data['results']:
         if i['id'] == parent_id:
             if i['id']!='2564620384':
                 path.insert(0, i['title'].replace(':','-').replace('/','-'))
-                return getParentTitle(i['parentId'])
+                return getParentTitle(data, i['parentId'])
             else:
                 return path
 
-def filterPages(data):
-    for i in data:
-        if i['status']!='archived' and i['parentId'] not in parent_ids: 
-            A
-            parent_ids.append(i['parentId'])
-
 def main():
     data = getConfluenceData()
-    filterPages(data)
-    for i in data:
+    filterParentPages(data)
+    for i in data['results']:
         if i['status']!='archived' and i['id'] not in parent_ids:
+            global path
             path = []
-            pages.append({'id':i['id'],'path':i['title']})
-            fullpath = 'Confluence/' + '/'.join(getParentTitle(i['parentId'])) + '/' + i['id']        
+            fullpath = 'Confluence/' + '/'.join(getParentTitle(data, i['parentId'])) + '/' + i['id']        
+
             output_file = Path(fullpath + '.md')
             output_file.parent.mkdir(exist_ok=True, parents=True)
             with open(output_file, 'w', encoding='utf-8') as f:
                 f.write(md(i['body']['storage']['value']))
+            
+            pages.append({'id':i['id'],'path':i['title']})
 
 main()
         
