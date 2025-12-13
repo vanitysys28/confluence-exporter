134c95f2d118d7f90752d9cfc82eaa242a0a263a
vanitysys28 <guiaubzer@gmail.com>
2025-12-13 18:31:22 +0100

    add proper string sanitizing

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import re
 from requests.auth import HTTPBasicAuth
 import json
 from dotenv import load_dotenv
@@ -12,7 +13,7 @@ parent_ids = []
 pages = []
 path = []
 
-def getConfluenceData():
+def getConfluenceSpacesData():
     url = os.getenv('URL')
     endpoint = '/wiki/api/v2/spaces/2564620362/pages?body-format=storage&limit=250'
     auth = HTTPBasicAuth(os.getenv('EMAIL'), os.getenv('TOKEN'))
@@ -26,40 +27,71 @@ def getConfluenceData():
 
     return data
 
+def getConfluenceFoldersData(folder):
+    url = os.getenv('URL')
+    endpoint = '/wiki/api/v2/folders/'
+    auth = HTTPBasicAuth(os.getenv('EMAIL'), os.getenv('TOKEN'))
+    
+    response = requests.get(url + endpoint + folder,auth=auth)
+    data = response.json()
+    
+    return data
+
 def filterParentPages(data):
     for i in data['results']:
         if i['status']!='archived' and i['parentId'] not in parent_ids: 
             parent_ids.append(i['parentId'])
 
 def filterBadCharacters(string):
-    characters = [':','/','<','>','*']
+    characters = [':','\\','/','<','>','*','"','|','?']
     for character in characters:
         if character in string:
-            string.replace(character,'-')
+            string = string.replace(character,'_')
     return string
 
 def getParentTitle(data, parent_id):
+    folders = [result['parentId'] for result in data['results'] if result.get('parentType') == 'folder']
+    
+    if parent_id in folders:
+        if getConfluenceFoldersData(parent_id)['parentId']!='2564620384':
+            path.insert(0, getConfluenceFoldersData(parent_id)['title'])
+            return getParentTitle(data, getConfluenceFoldersData(parent_id)['parentId'])
+        else:
+            path.insert(0, getConfluenceFoldersData(parent_id)['title'])
+            return path
+
     for i in data['results']:
         if i['id'] == parent_id:
             if i['id']!='2564620384':
-                path.insert(0, i['title'].replace(':','-').replace('/','-'))
+                path.insert(0, filterBadCharacters(i['title']))
                 return getParentTitle(data, i['parentId'])
             else:
-                return path
+                return path 
+
+def sanitizeConfluenceTags(content):
+    code_pattern = r'(?s)(<ac:structured-macro ac:name="code".*?CDATA\[)(.*?)(]]>.*?</ac:structured-macro>)'
+    image_pattern = r'(?s)(<ac:image .*?ac:src=)(.*?)(><ri:url.*?</ac:image>)'
+    sanitized_content = re.sub(code_pattern,r"<pre><code>\2</code></pre>",content)
+    sanitized_content = re.sub(image_pattern,r"<img src=\2></img>",sanitized_content)
+    return sanitized_content
 
 def main():
-    data = getConfluenceData()
+    data = getConfluenceSpacesData()
     filterParentPages(data)
     for i in data['results']:
         if i['status']!='archived' and i['id'] not in parent_ids:
             global path
             path = []
-            fullpath = 'Confluence/' + '/'.join(getParentTitle(data, i['parentId'])) + '/' + i['id']        
+            fullpath = 'Confluence/' + '/'.join(getParentTitle(data, i['parentId'])) + '/' + filterBadCharacters(i['title']) 
+            full_path_with_prefix = f"\\\\?\\{os.path.abspath(fullpath)}"       
 
-            output_file = Path(fullpath + '.md')
+            output_file = Path(full_path_with_prefix + '.md')
             output_file.parent.mkdir(exist_ok=True, parents=True)
+
             with open(output_file, 'w', encoding='utf-8') as f:
-                f.write(md(i['body']['storage']['value']))
+                confluence_content = i['body']['storage']['value']
+                sanitized_content = sanitizeConfluenceTags(confluence_content)
+                f.write(md(sanitized_content))
             
             pages.append({'id':i['id'],'path':i['title']})
 
